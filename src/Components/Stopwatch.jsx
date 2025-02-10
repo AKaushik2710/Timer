870a5b789f0a2f734631589dbeebdad8ae865021
AKaushik2710 <amankaushik2710ak@gmail.com>
2025-02-10 15:02:02 +0000

    Stopwatch Completed

--- src/Components/Stopwatch.jsx
+++ src/Components/Stopwatch.jsx
@@ -2,34 +2,45 @@ import { useEffect, useRef, useState } from "react";
 import Div from "./Div";
 import Para from "./Para";
 export default function Stopwatch(){
-    const [stopwatch, setStopwatch] = useState({time : 0, pause : false});
+    const [stopwatch, setStopwatch] = useState({seconds : 57, pause : false, min : 59, hour : 0});
     useEffect(()=>{
-        stopwatch.time < 61 ? setter() : setStopwatch({...stopwatch, time : 0});
-        return () => clearInterval(timeRef.current);
-    },[stopwatch.time])
+        let min = stopwatch.min;
+        if(++min === 61){
+            setStopwatch({...setStopwatch, min : 0, seconds : 0, hour : stopwatch.hour+1});
+            setter();
+        }
+        else{
+        stopwatch.seconds < 60 ? setter() : setStopwatch({...stopwatch, seconds : 0, min : stopwatch.min+1});
+        }
+        return () => clearInterval(secondsRef.current)
+    },[stopwatch.seconds])
 
-    let timeRef = useRef();
+    let secondsRef = useRef();
     function setter(){
-        timeRef.current = setInterval(()=>{
-            setStopwatch(stopwatch => {return {...stopwatch,time :stopwatch.time+1}});
+        secondsRef.current = setInterval(()=>{
+            setStopwatch(stopwatch => {return {...stopwatch,seconds :stopwatch.seconds+1}});
         },1000)
     }
+
     function handlePause(val){
         if(val){
             setStopwatch({...stopwatch, pause : true});
-            clearInterval(timeRef.current)
+            clearInterval(secondsRef.current)
         }
         else{
             setStopwatch({...stopwatch, pause : false})
             setter();
         }
     }
+    function handleRestart(){
+        setStopwatch({...stopwatch, seconds : 0, min : 0, hour : 0})
+    }
     return <>
     <Div id="stopwatch">
-        <Div id="display">{stopwatch.time}</Div>
+        <Div id="display">{stopwatch.hour}{":"}{stopwatch.min}{":"}{stopwatch.seconds}</Div>
         <Div id="functionality">
-            <Para id="pause" clickHandler={()=>handlePause(!stopwatch.pause)}>{"Pause"}</Para>
-            <Para id="restart" clickHandler={()=> setStopwatch({...stopwatch, time : 0})}>{"Restart"}</Para>
+            <Para id="pause" clickHandler={()=>handlePause(!stopwatch.pause)}>{stopwatch.pause ? "Play" : "Pause"}</Para>
+            <Para id="restart" clickHandler={handleRestart}>{"Restart"}</Para>
         </Div>
     </Div>
     </>
