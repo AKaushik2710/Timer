6ce094b4da65efb9773b81a7559f15ef22cf7c8a
AKaushik2710 <159285365+AKaushik2710@users.noreply.github.com>
2025-02-22 21:16:57 +0530

    Update Choose.jsx

--- src/Components/Choose.jsx
+++ src/Components/Choose.jsx
@@ -1,5 +1,7 @@
 import Div from "./Div"
 import Para from "./Para"
+
+// Selection Functionality between Timer & Stopwatch 
 export default function Chooser({handleSwap}){
     // handleChoices => Swapping Between Stopwatch & Timer
     return <>
