ed6dbdb9853dd002cc773374971da43576d6681f
AKaushik2710 <159285365+AKaushik2710@users.noreply.github.com>
2025-02-22 21:17:46 +0530

    Update Para.jsx

--- src/Components/Para.jsx
+++ src/Components/Para.jsx
@@ -1,3 +1,4 @@
+// Paragraph Component 
 export default function Para(props=undefined){
     const {id="", cn, clickHandler, children, style} = props;
     return <p id={id} className={cn} onClick={clickHandler} style={style}>{children}</p>
