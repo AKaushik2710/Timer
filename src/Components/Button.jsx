f3fe10d0a2d4c9fc8408001bf37c9e4054252b0b
AKaushik2710 <159285365+AKaushik2710@users.noreply.github.com>
2025-02-22 21:15:28 +0530

    Update Button.jsx

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,3 +1,4 @@
+// Button Component 
 export default function Button(props){
     const {id="", cn="", ds, children, clickHandler} = props;
     return <button id={id} className={cn} disabled={ds} onClick={clickHandler}>{children}</button>
