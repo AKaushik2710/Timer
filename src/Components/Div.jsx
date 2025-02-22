844bd7b8adcf1e639d9d47e28ea285708ed46ad1
AKaushik2710 <159285365+AKaushik2710@users.noreply.github.com>
2025-02-22 21:17:22 +0530

    Update Div.jsx

--- src/Components/Div.jsx
+++ src/Components/Div.jsx
@@ -1,3 +1,4 @@
+// Div Component 
 export default function Div(props){
     const {id, cn, clickHandler, children} = props;
     return <div id={id} className={cn} onClick={clickHandler}>{children}</div>
